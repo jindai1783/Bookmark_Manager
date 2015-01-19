2b9bd3a86cdddbacbd10c2ab743a9cbdb16a954d
Jin Dai <jindai1783@gmail.com>
2015-01-19 03:12:34 +0000

    Roughly done

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -1,12 +1,16 @@
-  post '/links' do
-    # puts '===' * 30
-    # puts params.inspect
-    # {"url"=>"www.makersacademy.com", "title"=>"Makers", "tags"=>"education ruby javascript"}
-    url = params["url"]
-    title = params["title"]
-    tags = params["tags"].split(' ').map do |tag|
-      Tag.first_or_create(:text => tag)
-    end
-    Link.create(:url => url, :title => title, :tags => tags)
-    redirect to('/')
+post '/links' do
+  # puts '===' * 30
+  # puts params.inspect
+  # {"url"=>"www.makersacademy.com", "title"=>"Makers", "tags"=>"education ruby javascript"}
+  url = params["url"]
+  title = params["title"]
+  tags = params["tags"].split(' ').map do |tag|
+    Tag.first_or_create(:text => tag)
   end
+  Link.create(:url => url, :title => title, :tags => tags)
+  redirect to('/')
+end
+
+get '/links/new' do
+  erb :"links/new"
+end
